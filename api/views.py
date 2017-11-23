affca9fa000009e28d2264e1e6afb114b4699051
Edmond Atto <edmond091@gmail.com>
2017-11-24 00:53:35 +0300

    [FEATURE] Addclass view that defines PUT, GET and DELETE

--- api/views.py
+++ api/views.py
@@ -11,3 +11,9 @@ class CreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         """Save post data when creating a new shopping list"""
         serializer.save()
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """A class that defines the GET, PUT and DELETE behaviour of the REST API"""
+    queryset = ShoppingList.objects.all()
+    serializer_class = ShoppingListSerializer
