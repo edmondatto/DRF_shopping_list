30fb4b57676f3d52029daad945af5bbd2c54d172
Edmond Atto <edmond091@gmail.com>
2017-11-24 00:08:50 +0300

    [FEATURE] Create class that defines the API's create behaviour

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import ShoppingList
+from .serializers import ShoppingListSerializer
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """A class that defines the create behaviour of the REST API"""
+    queryset = ShoppingList.objects.all()
+    serializer_class = ShoppingListSerializer
+
+    def perform_create(self, serializer):
+        """Save post data when creating a new shopping list"""
+        serializer.save()
