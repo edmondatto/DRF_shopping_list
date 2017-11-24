5a7993a08b8bf0d9871e31cbc2b6557750576ffc
Edmond Atto <edmond091@gmail.com>
2017-11-24 16:13:32 +0300

    [FEATURE] Create views for retrieving, registering and logging in user(s)

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from .models import ShoppingList, Items
-from .serializers import ShoppingListSerializer, ItemsSerializer
+from .serializers import ShoppingListSerializer, ItemsSerializer, UserSerializer
 from .permissions import IsOwner
 
 
@@ -48,3 +49,22 @@ class ItemDetailsView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.IsAuthenticated, IsOwner)
     lookup_field = 'id'
     lookup_url_kwarg = 'pk2'
+
+
+class UserView(generics.ListAPIView):
+    """A class that returns the user list"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """A class that returns a user list"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """A class that registers a new user"""
+    model = User
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
