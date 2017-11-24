1a93d8a05320a53de89dc5469c17fce40c4246da
Edmond Atto <edmond091@gmail.com>
2017-11-24 07:24:18 +0300

    [CHORE] Add owner details to serializer file

--- api/serializers.py
+++ api/serializers.py
@@ -5,10 +5,10 @@ from .models import ShoppingList
 class ShoppingListSerializer(serializers.ModelSerializer):
     """A model serializer to map the model instance into JSON format"""
 
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = ShoppingList
-        fields = ('id', 'name', 'date_created', 'date_modified')
+        fields = ('id', 'name', 'owner', 'date_created', 'date_modified')
         read_only_fields = ('date_created', 'date_modified')
-
-
