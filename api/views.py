1ee722138a720e1708e1ae38e51a97589ca6f401
Edmond Atto <edmond091@gmail.com>
2017-11-24 07:28:51 +0300

    [CHORE] Add IsOwner to the permissions_classes tuples

--- api/views.py
+++ api/views.py
@@ -1,19 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import ShoppingList
 from .serializers import ShoppingListSerializer
+from .permissions import IsOwner
 
 
 class CreateView(generics.ListCreateAPIView):
     """A class that defines the create behaviour of the REST API"""
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def perform_create(self, serializer):
         """Save post data when creating a new shopping list"""
-        serializer.save()
+        serializer.save(owner=self.request.user)
 
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """A class that defines the GET, PUT and DELETE behaviour of the REST API"""
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
