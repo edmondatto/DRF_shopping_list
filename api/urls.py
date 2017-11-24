edde4f5ba2546b925fb1f216b4d387d25b40b685
Edmond Atto <edmond091@gmail.com>
2017-11-24 15:24:40 +0300

    [CHORE] Add routing for the shopping list items

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from .views import ShoppingListView, ShoppingListDetailsView, ItemListView, ItemDetailsView
 
 urlpatterns = {
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^shoppinglists/$', CreateView.as_view(), name="create"),
-    url(r'^shoppinglists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
-    url(r'^get-token/', obtain_auth_token)
+    url(r'^shoppinglists/$', ShoppingListView.as_view(), name="shopping_lists"),
+    url(r'^shoppinglists/(?P<pk>[0-9]+)/$', ShoppingListDetailsView.as_view(), name="single_shopping_list"),
+    url(r'^shoppinglists/(?P<pk>[0-9]+)/items/$', ItemListView.as_view(), name="items"),
+    url(r'^shoppinglists/(?P<pk>[0-9]+)/items/(?P<pk2>[0-9]+)/$', ItemDetailsView.as_view(), name="single_item"),
+    url(r'^auth/login/', obtain_auth_token)
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
