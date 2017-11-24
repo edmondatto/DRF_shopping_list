fbd3193a30e1a7a89ee3814fd81ad041fbf93a3c
Edmond Atto <edmond091@gmail.com>
2017-11-24 16:38:06 +0300

    [CHORE] Make changes to admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import ShoppingList, Items
 
-# Register your models here.
+from rest_framework.authtoken.admin import TokenAdmin
+
+TokenAdmin.raw_id_fields = ('user',)
+
+admin.site.register(ShoppingList, Items, User)
