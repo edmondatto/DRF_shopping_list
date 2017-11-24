a1e5902bd09cd5c8607ff7043666fe3c1a15b865
Edmond Atto <edmond091@gmail.com>
2017-11-24 16:11:51 +0300

    [FEATURE] Create User serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import ShoppingList, Items
 
@@ -22,3 +23,25 @@ class ItemsSerializer(serializers.ModelSerializer):
         model = Items
         fields = ('id', 'name', 'shoppinglist', 'date_created', 'date_modified', 'bought')
         read_only_fields = ('date_created', 'date_modified')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """A model serializer to map the model instance into JSON format"""
+
+    shoppinglists = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email', 'shoppinglists')
+        write_only_fields = ('password', )
+        read_only_fields = ('id', )
+
+    def create(self, validated_data):
+        """A method that handles creation of a new user"""
+        new_user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        new_user.set_password(validated_data['password'])
+        new_user.save()
+        return new_user
